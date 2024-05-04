3d26ca88501c7161229ade5d760282c2e9ce1561
Lincoln Muller <thyssenkrupp234@gmail.com>
2024-05-04 09:39:28 -0700

    Add stable free

--- SeatScript.lua
+++ SeatScript.lua
@@ -1,1 +1,32 @@
-empty
+local realseat = script:WaitForChild("RealSeat").Value
+local CD = script.Parent
+local seatFilled = false
+
+local players = game:GetService("Players")
+
+script.Parent.MouseClick:Connect(function(player)
+    if seatFilled then return end
+    realseat.Disabled = false
+    realseat:Sit(player.Character:WaitForChild("Humanoid"))
+end)
+
+realseat:GetPropertyChangedSignal("Occupant"):Connect(function()
+    if realseat.Occupant then
+        handleUser(players:GetPlayerFromCharacter(realseat.Occupant.Parent))
+        seatFilled = true
+        CD.MaxActivationDistance = 0
+        script.Parent.Parent.Parent.FillColour.Value = Color3.fromRGB(245, 30, 0)
+    else
+        seatFilled = false
+        CD.MaxActivationDistance = 10
+        script.Parent.Parent.Parent.FillColour.Value = Color3.fromRGB(2, 255, 0)
+    end
+end)
+
+game["Run Service"].Heartbeat:Connect(function()
+    if not realseat.Occupant then
+        if realseat.Disabled == false then
+            realseat.Disabled = true
+        end
+    end
+end)
